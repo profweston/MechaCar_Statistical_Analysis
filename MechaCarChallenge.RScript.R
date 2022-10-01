de9520ed3749ba6b9f11c1298261947c428fb14b
profweston <108107856+profweston@users.noreply.github.com>
2022-09-30 21:49:04 -0500

    Adding Challenge Files

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,19 @@
+#Deliverable 1 for Module 15
+
+library(dplyr)
+
+sink("output.txt", append = T)
+
+mpg <- read.csv(file = 'MechaCar_mpg.csv', check.names=F, stringsAsFactors = F)
+
+lm (mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mpg)
+
+summary(lm (mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mpg))
+
+#Deliverable 2
+
+coil <- read.csv(file = 'Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+
+total_summary <- coil %>%  summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+lot_summary <- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
