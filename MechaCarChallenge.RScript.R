41cdc97c4274dcfcfc30419bbb1fc929af504db8
profweston <108107856+profweston@users.noreply.github.com>
2022-10-03 18:35:49 -0500

    Updating challenge files

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -17,3 +17,11 @@ coil <- read.csv(file = 'Suspension_Coil.csv', check.names=F, stringsAsFactors =
 total_summary <- coil %>%  summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
 
 lot_summary <- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+#Deliverable 3
+
+t.test(coil$PSI, mu=1500)
+
+t.test(subset(coil, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+t.test(subset(coil, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+t.test(subset(coil, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
